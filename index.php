ccf4ef5f7b7297eb90747e3e339d8fe3ce11b4ad
amichaudet <35701521+amichaudet@users.noreply.github.com>
2018-01-31 23:24:16 -0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=”utf-8” />
+        <title >Portfolio</title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+    <!--outer "div"-->
+    <body style="background-color:#6699ff;">
+    <center>
+    
+    <header> <h1><center> Alexandre MICHAUDET Portfolio</center> </h1></header>
+    
+    <div>
+    <nav> <hr width="50%"/>
+    <a href="index.html"> <u>Home</u> </a>
+     <a href="info.html"> Info </a>
+     <a href="contact.html"> Contact</a>
+    </nav>
+    </div>
+    </center>
+    <br />
+    <main>
+        <center>
+        <figure id="caca">
+            <img src="img/sea-otter.jpg" alt="Picture of an otter"  width="500">
+        </figure>
+        </center>
+         <div id="prout"> Hello, welcome to my online portfolio.<br/> I am a student in computer science.<br/>Please contact me if you need any information.      </div>
+    </main>
+
+   
+       <footer>
+            <hr>
+            CST336 : Internet Programming. 2017&copy; Jason Henderson <br/>
+            <strong>Disclaimer:</strong> This webpage is used for academic purposes only.<br/>
+            
+        </footer>
+    </body>
+</html>
---
+++ info.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+
+
+
+<html>
+    <head>
+        <meta charset=”utf-8” />
+        <
+        <title>Portfolio</title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+
+    </head>
+    <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+    <!--outer "div"-->
+    <body style="background-color:powderblue;">
+        
+            <center>
+    <header> <h1> <center> About Me</center> </h1></header>
+       <div> <nav> <hr width="50%"/>
+    <a href="index.html"> Home </a>
+     <a href="info.html"><u> Info</u> </a>
+     <a href="contact.html"> Contact</a>
+    </nav>
+    </div></center>
+    <div>
+    <br/>
+    I'm Alexandre Michaudet, I live in Paris and I am in an engineering school there.<br/> I am currently studying abroad at CSUMB for one semester.
+    </div> <div>
+    <br/>
+    <table>
+        <br/>
+  <tr>
+    <th class="row">Subject</th>
+    <th>Experience</th>
+    
+  </tr> 
+  <tr>
+    <td>C++ programming</td>
+    <td>1 year</td>
+
+  </tr>
+    <tr>
+    <td>HTML, JavaScript,  CSS</td>
+    <td>None</td>
+
+  </tr>
+    <tr>
+    <td>Python programming</td>
+    <td>1 year</td>
+
+  </tr>
+    <tr>
+    <td>C programming</td>
+    <td>2 years</td>
+
+  </tr>
+  </table> <br/>
+  </div>
+          <figure>
+            <img src="img/csumb.png" alt="Logo of CSUMB" width="229" height="83">
+        </figure>
+
+<footer>
+            <hr>
+            CST336 : Internet Programming. 2017&copy; Jason Henderson <br/>
+            <strong>Disclaimer:</strong>This webpage is used for academic purposes only.
+        </footer>
+    </body>
+</html>
