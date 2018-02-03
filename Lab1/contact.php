df3d6d34832ff4541fa62be97968cf540a74b22b
amichaudet <35701521+amichaudet@users.noreply.github.com>
2018-02-03 10:56:39 -0800

    Add files via upload

---
+++ Lab1/contact.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+
+
+<html>
+    <head>
+        <meta charset=”utf-8” />
+        <title>Contact</title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+             </head>
+    <!--outer "div"-->
+    <body style="background-color:#cc33ff;">
+        <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+        
+    <header> <h1> <center> Contact </center></h1></header>
+    <center>
+        <div>
+        <nav> <hr width="50%"/>
+    <a href="index.php"> Home </a>
+     <a href="info.php"> Info </a>
+     <a href="contact.php"><u> Contact</u></a>
+    </nav>
+    </center>
+    </div>
+    <br/>
+    
+    <div id="content">
+        <center>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdjQJ_nraZL5UW20hoF4VAtKxUF2SuhwsM7hfrBXjYFDnbizg/viewform?embedded=true" width="1000" height="500" frameborder="0" marginheight="0" marginwidth="0">Chargement en cours...</iframe>
+    </center>
+    </div>
+    <footer>
+            <hr>
+            CST336 : Internet Programming. 2017&copy; Jason Henderson <br/>
+            <strong>Disclaimer:</strong>This webpage is used for academic purposes only.
+        </footer>
+    
+    
+    </body>
+    </html>
