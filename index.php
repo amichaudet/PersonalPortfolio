986d6ff6a0d72acd43f6e10c142b830a19f335aa
amichaudet <35701521+amichaudet@users.noreply.github.com>
2018-02-25 21:31:11 -0800

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,7 @@
     <a href="Homework1"> <u>Homework 1</u> </a> <br>
     <a href="777"> Lab2 : Slot Machine </a> <br>
     <a href="Challenge"> <u>Challenge</u> </a>
-  //  <a href="Lab3"> <u>Lab3</u> </a>
-    <a href="Assignement2"> <u>Assignement 2</u> </a>
+<br> <a href="Assignement2"> <u>Assignement 2</u> </a>
      
 
 </body>
