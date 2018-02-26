e3d2a75d83237275f53a61b448d6c1e90080927d
amichaudet <35701521+amichaudet@users.noreply.github.com>
2018-02-25 21:28:24 -0800

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
     <a href="Lab1"> Portfolio Lab1 </a> <br>
     <a href="Homework1"> <u>Homework 1</u> </a> <br>
     <a href="777"> Lab2 : Slot Machine </a> <br>
+    <a href="Challenge"> <u>Challenge</u> </a>
+  //  <a href="Lab3"> <u>Lab3</u> </a>
+    <a href="Assignement2"> <u>Assignement 2</u> </a>
      
 
 </body>
