bc7139b93f503e4ed197cb0c640ac08256bdcc16
amichaudet <35701521+amichaudet@users.noreply.github.com>
2018-03-12 21:31:25 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,17 @@
 
 <h1>Alexandre MICHAUDET</h1>
 
-    <a href="Lab1"> Portfolio Lab1 </a> <br>
-    <a href="Homework1"> <u>Homework 1</u> </a> <br>
-    <a href="777"> Lab2 : Slot Machine </a> <br>
+    <a href="PersonalPortfolio"> Portfolio Lab1 </a>
+    <a href="Homework1"> <u>Homework 1</u> </a>
+    <a href="777"> <u>Lab 2: Slot machine</u> </a>
     <a href="Challenge"> <u>Challenge</u> </a>
-<br> <a href="Assignement2"> <u>Assignement 2</u> </a>
-   <br> <a href="Lab3"> <u>Lab3</u> </a>
+
+    <a href="Assignement2"> <u>Assignement 2</u> </a>
+    
+    <br>    <a href="Lab3.1"> <u>Lab3</u> </a> 
+     <a href="https://github.com/kevensiao/Lab3">  Link to the shared repository for lab 3</a>  
+    
+     <br>    <a href="Lab4"> <u>Lab4</u> </a> 
      
 
 </body>
