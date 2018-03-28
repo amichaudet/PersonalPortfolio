441b17e2843f22153a30dbf414ea19b829f82416
amichaudet <35701521+amichaudet@users.noreply.github.com>
2018-03-28 09:41:38 -0700

    Add files via upload

--- Midterm/program1.php
+++ Midterm/program1.php
@@ -29,6 +29,85 @@ session_start();
   </style>
 </head>
 <body>
+
+                                <table border="1" width="600">
+                                    <tbody><tr>
+                                        <th>#</th>
+                                        <th>Task Description</th>
+                                        <th>Points</th>
+                                    </tr>
+                                    <tr style="background-color:green">
+                                        <td>1</td>
+                                        <td>The page includes the basic form elements as in the Program Sample: Text boxes, Dropdown menu, submit button</td>
+                                        <td width="20" align="center">5</td>
+                                    </tr>
+                                    <tr style="background-color:green">
+                                        <td>2</td>
+                                        <td>When submitting the form, an error message is displayed if the product of columns and rows exceeds 39</td>
+                                        <td width="20" align="center">5</td>
+                                    </tr>
+                                    <tr style="background-color:green">
+                                        <td>3</td>
+                                        <td>When submitting the form, a table is created with random playing cards</td>
+                                        <td align="center">5</td>
+                                    </tr>
+                                    <tr style="background-color:green">
+                                        <td>4</td>
+                                        <td>The size of the table corresponds to the one selected by the user </td>
+                                        <td align="center">10</td>
+                                    </tr>
+                                    <tr style="background-color:green">
+                                        <td>5</td>
+                                        <td>The cards are NOT duplicated </td>
+                                        <td align="center">10</td>
+                                    </tr>
+                                    <tr style="background-color:green">
+                                        <td>6</td>
+                                        <td>No cards of the suit selected by the user are displayed in the game </td>
+                                        <td align="center">10</td>
+                                    </tr>
+                                    <tr style="background-color:red">
+                                        <td>7</td>
+                                        <td>The Aces have a yellow background</td>
+                                        <td align="center">5</td>
+                                    </tr>
+                                    <tr style="background-color:red">
+                                        <td>8</td>
+                                        <td>The Kings have a cyan background</td>
+                                        <td align="center">5</td>
+                                    </tr>
+                                    <tr style="background-color:green">
+                                        <td>9</td>
+                                        <td>The total number of Aces and Kings are displayed</td>
+                                        <td align="center">5</td>
+                                    </tr>
+                                    <tr style="background-color:green">
+                                        <td>10</td>
+                                        <td>A message indicates who won, Aces or Kings, (or neither) </td>
+                                        <td align="center">5</td>
+                                    </tr>
+                                    <tr style="background-color:green">
+                                        <td>11</td>
+                                        <td>At least five CSS rules are included</td>
+                                        <td align="center">5</td>
+                                    </tr>
+                                    <tr style="background-color:#99E999">
+                                        <td>12</td>
+                                        <td>This rubric is properly included AND UPDATED (BONUS)</td>
+                                        <td width="20" align="center">2</td>
+                                    </tr>
+                                    <tr>
+                                        <td></td>
+                                        <td>T O T A L </td>
+                                        <td width="20" align="center"><b></b></td>
+                                    </tr>
+                                </tbody></table>
+                                
+    
+    
+    
+    
+    
     <div id="wrapper">
         <header>
             <h1>Aces vs Kings</h1>
---
+++ Midterm/program2.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Midterm program2</title>
+    </head>
+    <body>
+                                    <table border=1 width="600">
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Task Description</th>
+                                        <th>Points</th>
+                                    </tr>
+                                    <tr style="background-color:green">
+                                        <td>1</td>
+                                        <td>A report shows all female students ordered by last name, from A to Z</td>
+                                        <td width="20" align="center">10</td>
+                                    </tr>
+                                    <tr style="background-color:green">
+                                        <td>2</td>
+                                        <td>A report shows students that have assignments with a grade lower than 50, ordered by grade, in ascending order</td>
+                                        <td width="20" align="center">15</td>
+                                    </tr>
+                                    <tr style="background-color:green">
+                                        <td>3</td>
+                                        <td>A report lists those assignments that have not been graded and their due date, ordered by due date, ascending</td>
+                                        <td width="20" align="center">15</td>
+                                    </tr>
+                                    <tr style="background-color:green">
+                                        <td>4</td>
+                                        <td>A report shows the Gradebook, which includes first name, last name, assignment title, and grade. It should be ordered by last name and assignment title </td>
+                                        <td align="center">15</td>
+                                    </tr>
+                                    <tr style="background-color:green">
+                                        <td>5</td>
+                                        <td>A report lists each student along with his/her average grade, ordered by average grade, from highest to lowest</td>
+                                        <td width="20" align="center">15</td>
+                                    </tr>
+                                    <tr style="background-color:green">
+                                        <td>6</td>
+                                        <td>This rubric is properly included AND UPDATED (BONUS)</td>
+                                        <td width="20" align="center">2</td>
+                                    </tr>
+                                    <tr>
+                                        <td></td>
+                                        <td>T O T A L </td>
+                                        <td width="20" align="center"><b></b></td>
+                                    </tr>
+                                </table>
+                            
+        
+        
+        <?php
+            $bdd= new PDO("mysql:host=localhost; dbname=midterm", 'amichaudet', 'amichaudet');
+        ?>
+        <div id="header">
+            <h1><u>Midterm Program2</u></h1>
+        </div>
+
+         <?php
+            $reponse = $bdd->query('SELECT * FROM m_students WHERE gender="F" ORDER BY lastName ASC');
+            
+            echo "<br><b><u>The female students are</u></b><br><br>" ;
+            
+            while ($donnees = $reponse->fetch())
+            {
+                echo $donnees['firstName'] . "   " ;
+                echo $donnees['lastName'] . "</br>"; 
+            }
+            $reponse = $bdd->query('SELECT firstName, lastName, grade FROM m_students NATURAL JOIN m_gradebook WHERE grade <50 ORDER BY grade ASC');
+            
+             echo "<br><b><u>List of students that have assignments with a grade lower than 50 </u></b><br><br>" ;
+             
+            while ($donnees = $reponse->fetch())
+            {
+                echo $donnees['firstName'] . "   " ;
+                echo $donnees['lastName'] . "   " ;
+                echo $donnees['grade'] . "</br>"; 
+            }
+            
+            $reponse = $bdd->query('SELECT title, dueDate, assignmentId FROM m_assignments WHERE NOT EXISTS (SELECT assignmentId FROM m_gradebook WHERE m_assignments.assignmentId = m_gradebook.assignmentId)');
+            echo "<br><b><u>List of assignements that have not been graded</u></b><br><br>" ;
+
+            while ($donnees = $reponse->fetch())
+            {
+                echo $donnees['title'] . "  ";
+                echo $donnees['dueDate'] . "</br>";
+            }
+            
+            $reponse = $bdd->query('SELECT firstName, lastName, title, grade FROM m_students NATURAL JOIN m_gradebook NATURAL JOIN m_assignments ORDER BY lastName');
+            echo "<br><b><u>gradebook</u></b><br><br>" ;
+    
+            while ($donnees = $reponse->fetch())
+            {
+                echo $donnees['firstName'] . "   ";
+                echo $donnees['lastName'] . "   ";
+                echo $donnees['title'] . "   ";
+                echo $donnees['grade'] . "</br>";
+            }
+            
+            $reponse = $bdd->query('SELECT studentId, firstName, lastName, sum(grade) FROM m_students NATURAL JOIN m_gradebook GROUP BY studentId ORDER BY sum(grade) DESC');
+            
+            echo "<br><b><u>List of average grade per student (average of the three assignments)</u></b><br><br>" ;
+           
+            while ($donnees = $reponse->fetch())
+            {
+                echo $donnees['studentId'] . "   ";
+                echo $donnees['firstName'] . "   ";
+                echo $donnees['lastName'] . "   ";
+                echo $donnees['sum(grade)']/3;
+                echo $avg . "</br>";
+            }
+
+            
+            $reponse->closeCursor(); 
+            ?>
+    </body>
+</html>
